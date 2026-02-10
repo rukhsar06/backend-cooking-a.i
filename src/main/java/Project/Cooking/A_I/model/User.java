0c12151449e210e7880517f13dbddb06ffa76abf
Erza <rukhsarshah852@gmail.com>
2026-02-11 01:39:23 +0530

    Fix Postgres reserved keyword: rename user table

--- src/main/java/Project/Cooking/A_I/model/User.java
+++ src/main/java/Project/Cooking/A_I/model/User.java
@@ -3,7 +3,7 @@ package Project.Cooking.A_I.model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
 
     @Id
@@ -19,21 +19,15 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    // getters & setters
     public Long getId() { return id; }
-    public String getUsername() {  return username; }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-
     public void setId(Long id) { this.id = id; }
 
-    public String getEmail() { return email; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
+    public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 
     public String getPassword() { return password; }
-
     public void setPassword(String password) { this.password = password; }
 }
